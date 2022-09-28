22f5a28853933f0d0b1edc76d5475fc50be9e96a
ming0830 <minsun0830@naver.com>
2022-09-28 21:48:01 +0900

    [MOD] 2022.09.28

--- JS100/038.js
+++ JS100/038.js
@@ -1,3 +1,17 @@
 const input = "97 86 75 66 55 97 85 97 97 95";
 const sortedInput = input.split(' ').sort((a,b)=>b-a);
-console.log(Math.max(...sortedInput))
+console.log(sortedInput)
+
+//3번째로 큰 수 찾기
+
+const set = new Set(sortedInput)
+const third = Array.from(set)[2];
+
+//1~3위 학생들의 수
+let count = 0
+for(let i =0;i<sortedInput.length;i++){
+  if(third <= sortedInput[i]) count++
+
+}
+
+console.log(count)
