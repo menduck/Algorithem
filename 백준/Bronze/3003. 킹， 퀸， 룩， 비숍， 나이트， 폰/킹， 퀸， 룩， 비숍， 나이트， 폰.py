6a432f157a6331b596a952a0fb6c4df687326729
minsun <39366835+menduck@users.noreply.github.com>
2023-01-05 17:43:25 +0900

    Mod: 추가 풀이 작성

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,2 +1,12 @@
+# 초기 답 - 성공
 a,b,c,d,e,f = map(int,input().split(' '))
 print(1-a,1-b,2-c,2-d,2-e,8-f, end = ' ')
+
+# 2번째 풀이 - 성공
+import sys
+
+userNums = list(map(int, sys.stdin.readline().strip().split(' ')))
+chessNums = [1,1,2,2,2,8] # 체스 숫자를 리스트에 담고
+
+for i in range(6):
+   print(chessNums[i] - userNums[i], end = " ") # 반복해서 각각 체스 넘버에서 유저의 체스 개수만큼 빼주고 출력함
