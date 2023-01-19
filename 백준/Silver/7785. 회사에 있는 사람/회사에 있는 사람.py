d05cf76859037c3800d7e7b3da09d99cb3dafd99
minsun <39366835+menduck@users.noreply.github.com>
2023-01-19 16:54:51 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 172 ms, Memory: 50412 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,9 +1,10 @@
 import sys
 T = int(sys.stdin.readline().strip())
+
 inout_dict = {}
 for _ in range(T):
   name, inout = sys.stdin.readline().strip().split()
   inout_dict[name] = inout
 
-in_user = {key : value for key, value in inout_dict.items() if value == 'enter'}
-print(*sorted(in_user.keys(), reverse=True), sep = '\n')
+in_users = [key for key, value in inout_dict.items() if value == 'enter']
+print(*sorted(in_users, reverse=True), sep = '\n')
