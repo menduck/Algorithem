b2953f9602dd845dd1b186640f4cfbe6e3998823
minsun <39366835+menduck@users.noreply.github.com>
2023-01-30 16:28:31 +0900

    [Bronze III] Title: 나는 요리사다, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -0,0 +1,8 @@
+import sys
+user,max_score = 0,0
+for user_num in range(1,6):
+    user_score = sum(map(int, sys.stdin.readline().split()))
+    if max_score < user_score:
+        user, max_score = user_num, user_score
+
+print(user, max_score)
