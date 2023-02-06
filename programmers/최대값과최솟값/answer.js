724dcdb3c799025c00d79f1bd04e6e25ea353fb5
ming0830 <minsun0830@naver.com>
2023-02-06 10:31:49 +0900

    style: TIL_kdt 저장소에서 알고리즘 관련 파일 옮김

--- programmers/최대값과최솟값/answer.js
+++
@@ -1,8 +0,0 @@
-
-function solution(s) {
-  let num = s.split(' ').map(v=>parseInt(v));
-  let min = Math.min(...num);
-  let max = Math.max(...num);
-  let answer = `${min} ${max}`;
-  return answer
-}
