c39a2865c699a661e356d9a7944ca9b2ab242e50
ming0830 <minsun0830@naver.com>
2023-02-18 19:01:44 +0900

    refactor: 코드리뷰 후 if문 정리 및 추가 코드 풀이 maxlen 정리

--- programmers/[1차]캐시/answer.py
+++ programmers/[1차]캐시/answer.py
@@ -26,6 +26,8 @@ dities = ["Jeju", "Pangyo", "NewYork", "newyork"]
 
 총 실행 시간 : 16
 '''
+
+# solution - 성공
 from collections import deque
 def solution(cacheSize, cities):
     stack = deque()
@@ -58,3 +60,73 @@ def solution(cacheSize, cities):
                 stack.append(city)
     return time
 
+
+# solution2 - 성공/if문 정리하기
+from collections import deque
+def solution(cacheSize, cities):
+    stack = deque()
+    time = 0
+
+    # 만약 캐시크기가 0이면 모든 요소들이 cache miss이다.
+    if cacheSize == 0:
+      return len(cities) * 5
+    
+    for city in cities:
+        # 대소문자 관계 없으므로 모든 요소 소문자로 변환
+        city = city.lower()
+
+        # 동일한 값이 있으면(cache hits) 1를 누적해주고 동일한 값을 제거한다.
+        if city in stack:
+            time += 1
+            stack.remove(city)
+            
+        # 동일한 값이 없고(cache miss) cache size보다 크면 오래된 데이터는 제거한다.
+        else:
+            time += 5
+            if len(stack) >= cacheSize :
+                    stack.popleft()
+        
+        # 새로운 도시를 스택에 추가한다.
+        stack.append(city)
+    return time
+
+#  - if문 중복된 코드 제거하기
+#   - 조건마다 마지막에 새로운 도시를 스택에 추가해줘야 되기때문에 조건문 밖에서 마지막에 추가를 해준다.
+
+# solution - 성공/maxlen 
+
+from collections import deque
+def solution(cacheSize, cities):
+    stack = deque([],cacheSize) 
+    time = 0
+
+    # 만약 캐시크기가 0이면 모든 요소들이 cache miss이다.
+    if cacheSize == 0:
+      return len(cities) * 5
+    
+    for city in cities:
+        # 대소문자 관계 없으므로 모든 요소 소문자로 변환
+        city = city.lower()
+
+        # 동일한 값이 있으면(cache hit) 1를 누적해주고 동일한 값을 제거한다.
+        if city in stack:
+            time += 1
+            stack.remove(city)
+            
+        # 동일한 값이 없으면(cache miss) 5를 누적해 준다.
+        else:
+            time += 5
+        
+        # 새로운 도시를 스택에 추가한다.
+        stack.append(city)
+    return time
+'''
+## maxlen
+> deque(iterable,maxlen)
+
+- deque 길이의 최댓값을 설정할 수 있다.
+- deque에  maxlen 이상의 값을 추가할 경우, maxlen 길이의 맞게 값이 삭제된다.
+    - append()로 값을 추가할 경우, 가장 오래된 데이터(맨 왼쪽 요소)가 삭제된다.
+    - appendleft()로 값이 추가할 경우, 가장 최신 데이터(맨 오른쪽 요소)가 삭제된다.
+
+'''
