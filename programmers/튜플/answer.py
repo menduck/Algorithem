0154493686be8c7dbc83ad8df31cfd574ac55eef
ming0830 <minsun0830@naver.com>
2023-02-19 21:47:48 +0900

    refactor: 코드리뷰 후 추가 풀이

--- programmers/튜플/answer.py
+++ programmers/튜플/answer.py
@@ -4,29 +4,40 @@
 - 하나씩 차집합 이어 붙이기
 '''
 
+# solution1 - 차집합 활용
 def solution(s):
     # 입력값 전처리
     s = s[2:-2] # 젤 앞뒤 괄호 삭제
     s_not_bracket = s.replace('{','').replace('}',' ')
     s_list = s_not_bracket.split(' ,')
     s_int = list(map(lambda x : list(map(int,x.split(','))),s_list))
-  
+
 
     s_int.sort(key = len)
 
     answer = s_int[0]
-    temp_set = set(s_int[0])
 
     for i in range(1,len(s_int)):
-        temp = set(s_int[i]) - temp_set
+        temp = set(s_int[i]) - set(answer)
         answer.append(*temp)
-        temp_set.add(*temp)
 
     return answer
         
         
+# solution2 - not in 활용
+def solution(s):
+    # 입력값 전처리
+    s = s[2:-2] # 젤 앞뒤 괄호 삭제
+    s_not_bracket = s.replace('{','').replace('}',' ')
+    s_list = s_not_bracket.split(' ,')
+    s_int = list(map(lambda x : list(map(int,x.split(','))),s_list))
 
-    
 
+    s_int.sort(key = len)
 
-solution("{{1,2,3},{2,1},{1,2,4,3},{2}}")
+    answer = []
+    for ele_s in s_int:
+        for ele in ele_s:
+            if ele not in answer:
+                answer.append(ele)
+    return answer
