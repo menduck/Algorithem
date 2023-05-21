8ee41c0046af62fab4015bf7687b6ea37481e187
ming0830 <minsun0830@naver.com>
2023-05-22 01:25:17 +0900

    style: 주석 추가 및 변수 누락 수정

--- 프로그래머스/lv2/42583. 다리를지나는트럭/다리를지나는트럭.js
+++ 프로그래머스/lv2/42583. 다리를지나는트럭/다리를지나는트럭.js
@@ -15,33 +15,28 @@
 
 function solution(bridge_length, weight, truck_weights){
   crossingTruck = Array.from({length: bridge_length}).fill(0)
+  // 다리를 건너는 트럭들의 무게 합
   let bridge_sum = 0;
+  let seconds = 0;
   
-  // 1초를 증가시키고, 맨 처음 트럭을 다리에 올린다.
-  answer++;
+  // 1초 시작
+  seconds++
   crossingTruck.shift();
-  bridge_sum += truck_weights[0];
-  crossingTruck.push(truck_weights.shift());
+  bridge_sum += truck_weights[0]
+  crossingTruck.push(truck_weights.shift())
   
-  // 대기 트럭 배열이 남아있거나 다리 위에 올라간 트럭 배열이 남아있는 동안,
-  while (bridge_sum > 0) {
-      // 우선 시간이 1초 지났을 때, 
-      answer++;
-      
-      // 큐의 맨 앞을 꺼내고, 
-      bridge_sum -= crossingTruck.shift();
-      
-      // 만약 현재 시점 다리 하중에 다음 트럭의 무게를 더해도 다리가 버틸 수 있다면?
-      if (truck_weights.length > 0 && bridge_sum + truck_weights[0] <= weight) {
-          // 다음 트럭을 다리 배열에 넣는다.
-          bridge_sum += truck_weights[0];
-          crossingTruck.push(truck_weights.shift());
-        } else {
-          crossingTruck.push(0);
-      }
-  }
-  
-  return answer;
+  while(bridge_sum > 0){
+    seconds++
+    bridge_sum -= crossingTruck.shift()
+    if((bridge_sum + truck_weights[0]) <= weight && truck_weights.length > 0){
+      bridge_sum += truck_weights[0]
+      crossingTruck.push(truck_weights.shift())
+    } else {
+      crossingTruck.push(0)
+    }
+  } return seconds
+}
+
   
 console.log(solution(2, 10, [7, 4, 5, 6]));
 // solution - 실패
@@ -74,6 +69,3 @@ console.log(solution(2, 10, [7, 4, 5, 6]));
 
 //   return secondsUnit.reduce((acc, cur) => acc+cur,0) + 1
 // }
-
-
-
