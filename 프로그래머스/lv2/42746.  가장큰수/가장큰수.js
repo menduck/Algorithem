0308e74957cd4bf310966ca0c34c0b375a2f3440
ming0830 <minsun0830@naver.com>
2023-05-27 23:34:05 +0900

    [Refactor] array => str 수정 및 추가 풀이

--- 프로그래머스/lv2/42746.  가장큰수/가장큰수.js
+++ 프로그래머스/lv2/42746.  가장큰수/가장큰수.js
@@ -27,3 +27,43 @@ function solution(numbers) {
 }
 
 console.log(solution([3, 30, 34]));
+
+// solution - 코드 개선
+// 문자열은 슬라이싱이 되므로 굳이 array형태로 안 만들어도 됨.
+
+function solution(numbers) {
+  if (numbers.every((v) => v === 0)) return '0';
+
+  // 문자열은 슬라이싱이 됨
+  const splitedNumbers = numbers.map((v) => v.toString());
+  return splitedNumbers
+    .sort((a, b) => {
+      const numA = a[0];
+      const numB = b[0];
+
+      if (numA > numB) return -1;
+      if (numA < numB) return 1;
+
+      const newA = a + b;
+      const newB = b + a;
+      if (newA > newB) return -1;
+      if (newA < newB) return 1;
+    })
+    .join('');
+}
+
+// solution - 추가 풀이
+// numbers의 원소는 0 이상 1,000 이하 => numbers의 원소를 최대 천자리를 만든다.(ex) 1 => 111) 그 다음 크기를 비교해서 정렬한다.
+function solution(numbers) {
+  if (numbers.every((v) => v === 0)) return '0';
+  const splitedNumbers = numbers.map((v) => v.toString());
+  return splitedNumbers
+    .sort((a, b) => {
+      const newA = a.repeat(3);
+      const newB = b.repeat(3);
+
+      if (newA > newB) return -1;
+      if (newA < newB) return 1;
+    })
+    .join('');
+}
