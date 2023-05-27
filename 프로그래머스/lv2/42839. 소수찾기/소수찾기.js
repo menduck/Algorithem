904a6c4b11abf17ec00aabf24214b0e18abb5af6
ming0830 <minsun0830@naver.com>
2023-05-28 01:39:19 +0900

    Add: lv2 소수찾기 순열/소수(에라토스테네스의체)

---
+++ 프로그래머스/lv2/42839. 소수찾기/소수찾기.js
@@ -0,0 +1,28 @@
+// 에라토스테네스의 체
+function isPrimeNumber(number) {
+  if (number <= 1) return false;
+  if (number % 2 === 0) return number === 2 ? true : false;
+
+  const sqrt = parseInt(Math.sqrt(number));
+  for (let i = 3; i <= sqrt; i += 2) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+// 순열
+function solution(numbers) {
+  const splitedNumbers = numbers.split('');
+  const result = new Set();
+  function permutation(permu, rests, result) {
+    rests.forEach((v, idx) => {
+      const rest = [...rests.slice(0, idx), ...rests.slice(idx + 1)];
+      permutation([...permu, v], rest, result);
+    });
+    if (rests.length<splitedNumbers.length) return result.add(parseInt(permu.join(''),10));
+  };
+  permutation([], splitedNumbers, result);
+  return Array.from(result).filter((v) => isPrimeNumber(v)).length
+}
+
+console.log(solution('123'));
